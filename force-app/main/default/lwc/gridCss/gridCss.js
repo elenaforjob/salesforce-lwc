d996ce7d3bbe95dd5d4fe8b0a6e734a11e7bb985
elenaforjob <elenaforjob708@gmail.com>
2025-06-02 21:49:21 +0300

    css grid

---
+++ force-app/main/default/lwc/gridCss/gridCss.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class GridCss extends LightningElement {
+    card1 = {title: 'New leads', number: 245, description: 'This month'};
+    card2 = {title: 'Closed deals', number: 48, description: 'Last month'};
+    card3 = {title: 'Total revenue', number: 245000, description: 'This year'};
+    card4 = {title: 'Open cases', number: 35, description: 'This week'};
+}
