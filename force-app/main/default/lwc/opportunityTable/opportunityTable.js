58c50b464ff97807cf83381eb8a520cfb84ed2f9
elenaforjob <elenaforjob708@gmail.com>
2025-06-03 22:07:12 +0300

    Rework table to use for:each

--- force-app/main/default/lwc/opportunityTable/opportunityTable.js
+++ force-app/main/default/lwc/opportunityTable/opportunityTable.js
@@ -1,28 +1,11 @@
 import { LightningElement } from 'lwc';
 
 export default class OpportunityTable extends LightningElement {
-    task1 = 'BV upgrade';
-    status1 = 'Prospecting';
-    price1 = '$600';
-    date1 = '2024-05-16';
-
-    task2 = 'Olapic integration';
-    status2 = 'Closed Won';
-    price2 = '$2000';
-    date2 = '2024-05-15';
-
-    task3 = 'Implement cartridge';
-    status3 = 'Closed Lost';
-    price3 = '$3000';
-    date3 = '2024-06-18';
-
-    task4 = 'Customize checkout';
-    status4 = 'Prospecting';
-    price4 = '$5000';
-    date4 = '2024-08-07';
-
-    task5 = 'Customize PDP';
-    status5 = 'Closed Won';
-    price5 = '$4500';
-    date5 = '2024-03-15';
+    opportunities = [
+        { task: "BV upgrate", status: "Prospecting", price: "$600", date: "2024-05-16", style: "color-orange" },
+        { task: "Olapic integration", status: "Closed Won", price: "$2000", date: "2024-05-18", style: "color-green" },
+        { task: "Implement cartridge", status: "Closed Lost", price: "$500", date: "2024-05-14", style: "color-red" },
+        { task: "Customize checkout", status: "Prospecting", price: "$800", date: "2024-07-16", style: "color-orange" },
+        { task: "Customize PDP", status: "Closed Won", price: "$500", date: "2024-05-16", style: "color-green" }
+    ];
 }
